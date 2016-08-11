e7e08db29fc00a3b83c93970e4dc340fd30ef032
sieren <matthias@s-r-n.de>
2016-08-11 18:49:26 +0200

    Cleanup: Move private functions into private
    Those shouldnt be there.

--- includes/qst/syncwebview.h
+++ includes/qst/syncwebview.h
@@ -38,9 +38,7 @@ public:
   SyncWebView(QUrl url, Authentication authInfo);
   ~SyncWebView() = default;
 
-  void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
   void show();
-  virtual void contextMenuEvent(QContextMenuEvent * event) override;
   void updateConnection(QUrl url, Authentication authInfo);
 
 signals:
@@ -50,7 +48,9 @@ private:
   void initWebView();
   void setupMenu();
   void pageHasLoaded(bool hasLoaded);
-  
+  void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
+  virtual void contextMenuEvent(QContextMenuEvent * event) override;
+
   std::unique_ptr<SyncWebPage> mpPageView;
   QUrl mSyncThingUrl;
   Authentication mAuthInfo;
