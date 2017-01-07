33bcd28cd7ae9e3251e47fe6aa34e42f2078e724
Marco Molteni <marco.molteni@laposte.net>
2017-01-07 19:17:09 +0100

    Handle version information only in one place (DRY) - put version information in CMakeLists.txt - add templatized config.h and Info.plist - actually copy Info.plist into bundle - actually copy icon file into bundle (this makes the icon to show correctly In Finder)

---
+++ includes/config.template.h
@@ -0,0 +1,7 @@
+
+/*
+ * See https://cmake.org/cmake/help/v3.5/command/configure_file.html
+ */
+
+#define QSYNCTHINGTRAY_VERSION         "@QSYNCTHINGTRAY_VERSION@"
+#define QSYNCTHINGTRAY_COPYRIGHT_YEARS "@QSYNCTHINGTRAY_COPYRIGHT_YEARS@"
